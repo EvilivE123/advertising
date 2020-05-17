a0973b8759faa538d76ba9be451ba56e9f9156e6
Gaurav <palande.gaurav777@gmail.com>
2020-05-17 13:21:44 +0530

    master => 17th May, 2020 1:22PM

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,14 +96,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    domain: 'heroku.com',
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    authentication: :login,
-    enable_starttls_auto: true
-  }  
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'evilive-advertising-management.heroku.com',
+    :authentication => :plain,
+  }
   
   Rails.application.config.middleware.use ExceptionNotification::Rack,
   email: {
